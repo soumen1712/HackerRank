13bc49114e94b0df6da168a0ebcc1bb033608545
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2021-07-24 01:09:11 +0530

    Functions

---
+++ Functions.cpp
@@ -0,0 +1,34 @@
+//  Program Link: 
+//  https://www.hackerrank.com/challenges/c-tutorial-functions/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+//  Created by KUNTAL OJHA RAJ on 24/07/2021.
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d)
+{
+    if(a>b && a>c && a>d)
+    {
+        return a;
+    } else if(b>a && b>c && b>d)
+    {
+        return b;
+    } else if(c>b && c>a && c>d)
+    {
+        return c;
+    } else
+    {
+        return d;        
+    }
+}
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
