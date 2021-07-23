eb33cf2005bc5272ca86c47e9cf21212e165ece4
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2021-07-23 20:40:25 +0530

    Sum and Difference of Two Numbers

---
+++ Sum_and_Difference_of_Two_Numbers.c
@@ -0,0 +1,13 @@
+//  Program Link: 
+//  https://www.hackerrank.com/challenges/sum-numbers-c/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+//  Created by KUNTAL OJHA RAJ on 23/07/2021.
+
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+    float e,f;
+    scanf("%d%d%f%f",&a,&b,&e,&f);
+    printf("%d %d\n%.1f %.1f",a+b,a-b,e+f,e-f);
+    return 0;
+}
