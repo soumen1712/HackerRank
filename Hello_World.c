5fa82e7ce84f8e113b45cbefe002527adfa0a6f7
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2021-07-23 20:20:17 +0530

    "Hello World!" in C

---
+++ Hello_World.c
@@ -0,0 +1,12 @@
+//  Program Link: 
+//  https://www.hackerrank.com/challenges/hello-world-c/problem
+//  Created by KUNTAL OJHA RAJ on 23/07/2021.
+
+#include <stdio.h>
+int main() 
+{
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	printf("Hello, World!\n%s",s);   
+    return 0;
+}
