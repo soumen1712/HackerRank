51dd16b520bdc216771bc63676cac203bc148b91
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2021-07-23 20:29:31 +0530

    Playing With Characters

---
+++ Playing_With_Characters.c
@@ -0,0 +1,16 @@
+//  Program Link: 
+//  https://www.hackerrank.com/challenges/playing-with-characters/problem?h_r=next-challenge&h_v=zen
+//  Created by KUNTAL OJHA RAJ on 23/07/2021.
+
+#include <stdio.h>
+int main() 
+{
+    char a[100];
+    char b[100];
+    char c[100];
+    scanf("%[^\n]%*c", &a);
+    scanf("%[^\n]%*c", &b);
+    scanf("%[^\n]%*c", &c);
+    printf("%s\n%s\n%s",a,b,c);     
+    return 0;
+}
