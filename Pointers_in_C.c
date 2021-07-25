b5ba829e7634ce7c5d145f556d63f6c16a787ba9
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2021-07-25 23:56:13 +0530

    Pointer in C Method 1 to 6
    Leet Us, C Pages 157

--- Pointers_in_C.c
+++ Pointers_in_C.c
@@ -1,7 +1,8 @@
 //  Program Link: 
 //  https://www.hackerrank.com/challenges/pointer-in-c/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 //  Created by KUNTAL OJHA RAJ on 23/07/2021.
-// Pointers in C
+//  Pointers in C
+//  Leet Us, C Pages 157 (Call by referance)
 //------------------------------------------------------------- Method 01 -------------------------------------------------------------------
 
 
@@ -21,7 +22,8 @@ int main() {
 
 
 //------------------------------------------------------------- Method 02 -------------------------------------------------------------------
-
+//  Created by KUNTAL OJHA RAJ on 25/07/2021.
+// Pointers in C
 
 #include <stdio.h>
 
@@ -51,4 +53,85 @@ int main() {
 
 
 //------------------------------------------------------------- Method 03 -------------------------------------------------------------------
+//  Created by KUNTAL OJHA RAJ on 25/07/2021.
+// Pointers in C
 
+#include<stdio.h>
+
+void update(int *a, int *b){
+    int sum= *a + *b;
+    *b = *a - *b;
+    if( *b < 0 ){
+        *b = *b * -1;
+    }
+    *a = sum;
+}
+
+int main()
+{
+     int a, b;
+        int *pa = &a, *pb = &b;
+    scanf("%d %d", &a, &b);
+        update(pa, pb);
+            printf("%d\n%d", a, b);
+    return 0;
+}
+
+
+//------------------------------------------------------------- Method 04 -------------------------------------------------------------------
+//  Created by KUNTAL OJHA RAJ on 25/07/2021.
+// Pointers in C
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int c=(*a)+(*b);
+    int d=(*a)-(*b);
+    if(d<0)
+    {
+        d=d*(-1);
+    }
+    *a=c;
+    *b=d;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
+
+//------------------------------------------------------------- Method 05 -------------------------------------------------------------------
+//  Created by KUNTAL OJHA RAJ on 25/07/2021.
+// Pointers in C
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int c=(*a)+(*b);
+    *b=(*a)-(*b);
+    *a=c;
+    if(*b<0)
+    {
+        *b=*b*(-1);
+    }
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
