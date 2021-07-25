299bd6e63baeecb472bd7263d487d3b57de32c76
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2021-07-25 21:27:02 +0530

    Pointers in C Method 1 & 2
    Let Us C Page 156

--- Pointers_in_C.c
+++ Pointers_in_C.c
@@ -1,6 +1,9 @@
 //  Program Link: 
 //  https://www.hackerrank.com/challenges/pointer-in-c/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 //  Created by KUNTAL OJHA RAJ on 23/07/2021.
+// Pointers in C
+//------------------------------------------------------------- Method 01 -------------------------------------------------------------------
+
 
 #include <stdio.h>
 int main() {
@@ -15,3 +18,37 @@ int main() {
     printf("%d\n%d", c, d);
     return 0;
 }
+
+
+//------------------------------------------------------------- Method 02 -------------------------------------------------------------------
+
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int c=*a,d=*b,e,f;
+       e=c+d;
+       f=c-d;
+       if(f<0)
+       {
+           f=f*(-1);
+       }
+       *a=e;
+       *b=f;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
+
+//------------------------------------------------------------- Method 03 -------------------------------------------------------------------
+
