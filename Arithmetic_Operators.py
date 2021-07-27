5a60ad5f23e701909d6f5680f977c9bc2c1983da
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2021-07-27 15:29:03 +0530

    Arithmetic Operators

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,12 @@
+#  Program Link: 
+#  https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+#  Created by KUNTAL OJHA  on 27/07/2021.
+
+a = int(input())
+b = int(input())
+c = a+b
+d = a-b
+e = a*b
+print(c)
+print(d)
+print(e)
