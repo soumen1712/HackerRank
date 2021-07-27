ae58065c3755324c573e49e38e334fe1e3d1ecfc
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2021-07-27 15:33:36 +0530

    Python: Division

---
+++ Python_Division.py
@@ -0,0 +1,10 @@
+#  Program Link: 
+#  https://www.hackerrank.com/challenges/python-division/problem
+#  Created by KUNTAL OJHA  on 27/07/2021.
+
+# Created by KUNTAL OJHA  on 27/07/2021.
+a = int(input())
+b = int(input())
+c = a/b
+print(int (c))
+print(c)
